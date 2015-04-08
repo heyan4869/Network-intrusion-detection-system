fe8b6593c55b2cf11997f5ce14d04f3a0c7f822d
Yan <heyan4869@gmail.com>
2015-04-08 15:13:15 -0400

    classify test data with nbClassifier

--- fileReader.R
+++ fileReader.R
@@ -1,4 +1,3 @@
 # read the training data
-trainData = read.csv(path, header = FALSE);
-# show(trainData);
-return(trainData);
+data = read.csv(path, header = FALSE);
+return(data);
--- nbClassifier.R
+++ nbClassifier.R
@@ -1,15 +1,22 @@
-# train a naive bayes classifier with small portion
-# of training data
+# train a naive bayes classifier
 library("e1071")
 
-path = "kddcup.data_10_percent";
-trainData = fileReader(path);
-model <- naiveBayes(V42 ~ ., data = trainData);
-predict(model, trainData[1:30,]);
-predict(model, trainData[1:30,], type = "raw");
-pred <- predict(model, trainData);
-table(pred, trainData[, 42]);
+# read the train and test data
+trainPath = "dataset/kddcup.data_10_percent";
+testPath = "dataset/kddcup.testdata.unlabeled_10_percent"
+trainData = read.csv(trainPath, header = FALSE);
+testData = read.csv(testPath, header = FALSE);
 
+# train the naive bayes classifier with train data
+model <- naiveBayes(V42 ~ ., data = trainData);
+
+# classify the test data
+predict(model, testData);
+predict(model, testData, type = "raw");
+pred <- predict(model, trainData);
+# table(pred, trainData[, 42]);
+
+# calculate the accuracy
 count = 0;
 for (i in 1: length(pred)) {
     if (pred[i] == trainData[i, 42]) {
